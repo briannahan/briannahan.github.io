0be6564b3b4085edddab014a4f3c9608c164d926
briannahan <49705223+briannahan@users.noreply.github.com>
2020-11-20 15:16:21 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 var adminpassword = 'ResearchDragon';
+var studentDummyUser = "bhan";
+var studentDummyPw = "hi";
 
-function login(){
+function adminlogin(){
   var password = document.getElementById("password").value;
   if(console.log(password===adminpassword)){
     location.assign("viewrequests.html");
   }
   else{
-    location.assign("viewrequests.html");
+    location.assign("adminlogin.html");
   }
 }
 
+function studentlogin(){
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  if(console.log(password===studentDummyUser)&&console.log(password===studentDummyPw)){
+    location.assign("viewrequests.html");
+  }
+  else{
+    location.assign("studentlogin.html");
+  }
+}
